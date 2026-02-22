e1ff56bf6197a8cae29f158a6be4c942550dc2fa
Oren Buzaglo <orenbu10@gmail.com>
2026-02-22 11:50:37 +0200

    52 cards deck created

--- main.c
+++ main.c
@@ -9,18 +9,21 @@
 
 int main(void) {
  
-    /*  Initialaizing     
-    
-    
-    
-    
-    
-    
-    */
    // Initialaizing cards pack,player's hand and dealer's hand linked lists 
-    node *cards = NULL;
-    node *pl_hand = NULL;
-    node *dl_hand = NULL;
-    add_to_end(&cards,10);
-    return 0;
+    node *deck = NULL;
+    
+    //node *pl_hand = NULL;
+    //node *dl_hand = NULL;
+    uint8_t suit = 1;
+   // Initialaizing the 52 cards in the linked list 
+   while (suit <= 8) {
+        uint8_t rank = 0;
+        while (rank <=12) {
+        uint8_t fixed = (rank << 4) | suit ;
+        add_to_end(&deck,fixed);
+        rank++;
+        }
+        suit= suit<<1;
+    }
+    print_list(deck);
 }
