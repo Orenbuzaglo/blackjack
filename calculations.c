e4afd40db582cd03d274dbdab95f4c7f3228b48a
Oren Buzaglo <orenbu10@gmail.com>
2026-02-26 21:40:15 +0200

    fixes

--- calculations.c
+++ calculations.c
@@ -14,29 +14,29 @@ void decode_print(node** hand,Calc* game,int is_dealer) {
     uint8_t rawdata = ((*hand)->data);
     uint8_t rank = rawdata >> 4;
     uint8_t suit = rawdata & 0xf ;
+    
   
 switch (rank) {
     // Ace //
     case 1: {
         game->pl_ace_count += 1;
         game->pl_sum += 11;
-        //game->pl_sum += ((game->pl_sum) + 11 > 21) ? 1 : 11 ;
         printf("Ace of ");
     }
     break;
-    // Jack //
+    
     case 11: {
         printf ("Jack of ");
         game->pl_sum += 10 ;
     }
     break;
-    // Queen //
+    
     case 12: {
         printf ("Queen of ");
         game->pl_sum += 10 ;
     }
     break;
-    // King //
+    
     case 13: {
         printf ("King of ");
         game->pl_sum += 10 ;
@@ -57,3 +57,4 @@ case 4: printf("Diamonds"); break;
 case 8: printf("Spades");   break;
 }
 }
+
